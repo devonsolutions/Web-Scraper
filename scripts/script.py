a96c86fec6a53a979699b7ccad231716350ebfd6
CacheOutDev <cache-out@outlook.com>
2025-03-19 16:18:17 -0700

    On branch main  Your branch is ahead of 'origin/main' by 2 commits.    (use "git push" to publish your local commits) Changes to be committed: 	new file:   CarsData1.xlsx 	new file:   Documents.xlsx 	modified:   scripts/script.py

--- scripts/script.py
+++ scripts/script.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
 import re
+
 
 URL = "https://www.csustan.edu/financial-support-services/procurement-contract-services/bid-bid-results"
 page = requests.get(URL)
@@ -8,17 +11,27 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
 drupalDocuments = soup.find_all(href=re.compile("/sites"))
-# print(drupalDocuments)
+print(drupalDocuments)
 
 sharepointDocuments = soup.find_all(href=re.compile("/StanStatePublicDocs"))
-# print(sharepointDocuments)
+print(sharepointDocuments)
 
+# creating the DataFrame
+documents = pd.DataFrame({'Cars': ['BMW', 'Audi', 'Bugatti',
+                                   'Porsche', 'Volkswagen'],
+                          'MaxSpeed': [220, 230, 240, 210, 190],
+                          'Color': ['Black', 'Red', 'Blue',
+                                    'Violet', 'White']})
 
+# writing to Excel
+datatoexcel = pd.ExcelWriter('Documents.xlsx')
 
-# .prettify() doesn't work because findAll returns a list of match tags
+# write DataFrame to excel
+documents.to_excel(datatoexcel)
 
-# Scraping Multiple Web Pages
-# https://www.freecodecamp.org/news/how-to-scrape-multiple-web-pages-using-python/
+# save the excel
+datatoexcel.close()
+print('DataFrame is written to Excel File successfully.')
 
-# Handling Network Errors, Politeness and Rate Limiting 
-# https://medium.com/@spaw.co/how-to-scrape-multiple-pages-using-beautifulsoup-42d944847fac
+# https://stackoverflow.com/questions/34997174/how-to-convert-list-of-model-objects-to-pandas-dataframe
+# https://stackoverflow.com/questions/6750240/how-to-do-re-compile-with-a-list-in-python 
