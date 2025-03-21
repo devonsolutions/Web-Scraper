79d7290d33714aa00f7891b08faf1a80329e3cd2
CacheOutDev <cache-out@outlook.com>
2025-03-21 15:36:16 -0700

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   .DS_Store 	deleted:    CarsData1.xlsx 	deleted:    Documents.xlsx 	new file:   Links.xlsx 	modified:   scripts/script.py

--- scripts/script.py
+++ scripts/script.py
@@ -3,35 +3,43 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 import re
-
+import openpyxl
+from openpyxl.styles import PatternFill
 
 URL = "https://www.csustan.edu/financial-support-services/procurement-contract-services/bid-bid-results"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-drupalDocuments = soup.find_all(href=re.compile("/sites"))
-print(drupalDocuments)
+totalLinks = []
+totalNames = []
 
-sharepointDocuments = soup.find_all(href=re.compile("/StanStatePublicDocs"))
-print(sharepointDocuments)
+drupalLinks = soup.find_all(href=re.compile("/sites"))
 
-# creating the DataFrame
-documents = pd.DataFrame({'Cars': ['BMW', 'Audi', 'Bugatti',
-                                   'Porsche', 'Volkswagen'],
-                          'MaxSpeed': [220, 230, 240, 210, 190],
-                          'Color': ['Black', 'Red', 'Blue',
-                                    'Violet', 'White']})
+for link in drupalLinks:
+    totalLinks.append(link.get('href'))
 
-# writing to Excel
-datatoexcel = pd.ExcelWriter('Documents.xlsx')
+sharepointLinks = soup.find_all(href=re.compile("/StanStatePublicDocs"))
 
-# write DataFrame to excel
-documents.to_excel(datatoexcel)
+for link in sharepointLinks:
+    totalLinks.append(link.get('href'))
 
-# save the excel
-datatoexcel.close()
-print('DataFrame is written to Excel File successfully.')
+totalLinks = pd.Series(totalLinks, name="Link Addresses")
+
+
+totalLinks = totalLinks.to_excel("Links.xlsx")
+print('The links were documented in the Excel File successfully.')
+
+'''
+drupalLinks = pd.Series((drupalLinks), name="Drupal Documents")
+sharepointLinks = pd.Series((sharepointLinks), name="SharePoint Documents")
+
+result = (pd.concat([drupalLinks, sharepointLinks], axis=1)).to_excel("Links.xlsx")
+'''
 
 # https://stackoverflow.com/questions/34997174/how-to-convert-list-of-model-objects-to-pandas-dataframe
-# https://stackoverflow.com/questions/6750240/how-to-do-re-compile-with-a-list-in-python 
+# https://stackoverflow.com/questions/6750240/how-to-do-re-compile-with-a-list-in-python'
+# https://stackoverflow.com/questions/18062135/combining-two-series-into-a-dataframe-in-pandas
+# https://www.geeksforgeeks.org/how-to-add-colour-to-excel-cells-using-python/
+# https://docs.python.org/3/library/unittest.html
+# https://www.geeksforgeeks.org/how-to-add-colour-to-excel-cells-using-python/
