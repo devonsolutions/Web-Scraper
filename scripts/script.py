ce96d50e03a469d0b93d770113c8faa22d57ca7e
CacheOutDev <cache-out@outlook.com>
2025-04-28 15:52:35 -0700

    Added pagination for parent pages

--- scripts/script.py
+++ scripts/script.py
@@ -9,50 +9,42 @@ department_address = input("Enter the department link address ")
 page = requests.get(department_address)
 soup = BeautifulSoup(page.content, "html.parser")
 
-parent_pages, child_pages = [], []
-
-def findNavLinks():
-    # navBar_links = soup.find_all(class_="nav navbar-nav")
-    # navItem_links = nav_links.find_all(class_="nav-item")
-    # after cutting down size of links, look for links that match the re pattern
-    department_url = re.split(r'/', department_address)
-    department_url = department_url.pop(3)
-    parent_patterns = re.compile(r'/'+ department_url +'/')
-    parent_links = soup.find_all(href=parent_patterns)
-
-    for parent_link in parent_links:
-        parent_pages.append(parent_link.get('href'))
-        
-    print(parent_pages)
-
-'''
-
+parent_pages = []
+# child_pages = []
 link_name, link_address, migration_status, deletion_status = [], [], [], []
 
-link_patterns = re.compile(r'sites|StanStatePublicDocs')
-relevant_links = soup.find_all(href=link_patterns)
-
+def findNavLinks():
+    href_pattern = "https://csustan.edu"
+    newhrefpattern = department_address.slice(href_pattern)
+    print(newhrefpattern)
+    #navBar_pattern = re.compile(href_pattern)
+    #navBar_links = soup.find_all(href=navBar_pattern) 
+    #print(navBar_links) 
+'''
+    for navBar_link in navBar_links:
+        navBar_link = navBar_link.get('href')
+        navBar_link = "https://www.csustan.edu" + navBar_link
+        parent_pages.append(navBar_link)
 def addColumnValues():
+    link_patterns = re.compile(r'sites|StanStatePublicDocs')
+    relevant_links = soup.find_all(href=link_patterns)
+
     if len(relevant_links) != 0:
         for relevant_link in relevant_links:
             link_name.append(relevant_link.get_text())
             link_address.append(relevant_link.get('href'))
             migration_status.append(' ')
             deletion_status.append(' ')
-            parent_pages.append(' ') # remove once pagination is set
-            child_pages.append(' ') # remove once pagination is set
     else:
         link_name.append('No links present on this page.')
         link_address.append(' ')
         migration_status.append(' ')
         deletion_status.append(' ')
-        parent_pages.append(' ') # remove once pagination is set
-        child_pages.append(' ') # remove once pagination is set
         print("No links present on this page.")
 
 def createDataFrame():
     df = pd.DataFrame(parent_pages, columns = ['Parent Pages'])
-    df['Child Pages'] = child_pages
+    # df['Child Pages'] = child_pages
     df['Link Name'] = link_name
     df['Link Address'] = link_address
     df['Migrated to SP'] = migration_status
@@ -62,9 +54,11 @@ def createDataFrame():
     excel_name = excel_html.get_text()
 
     df = df.to_excel(excel_name + ".xlsx")
-
-addColumnValues()
-createDataFrame()
-'''
+    '''
 
 findNavLinks()
+
+#for parent_page in parent_pages:
+    #addColumnValues()
+
+#createDataFrame()
