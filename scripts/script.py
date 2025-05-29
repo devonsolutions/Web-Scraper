8f0f4972f16835929f23fcbf278cfa46b3c48a61
CacheOutDev <cache-out@outlook.com>
2025-05-29 16:37:49 -0700

    Filtered just the Nav Links
    Need to filter list to pinpoint just the nav links

--- scripts/script.py
+++ scripts/script.py
@@ -7,25 +7,60 @@ import openpyxl
 import os
 
 department_address = input("Enter the department link address ")
+
 page = requests.get(department_address)
 soup = BeautifulSoup(page.content, "html.parser")
 
+nav_hrefs = []
+
+nav_links = soup.find_all(class_="nav-link")
+
+for nav_link in nav_links:
+    nav_hrefs.append(nav_link.get('href'))
+
+#print(nav_hrefs)
+
+parent_pages = []
+
+for nav_href in nav_hrefs:
+    if nav_href:
+       parent_pages.append(nav_href)
+    else:
+        pass
+    print(nav_href)  
+
+print(parent_pages)
+
+'''
+
 link_name, link_address, migration_status, deletion_status = [], [], [], []
 
-link_patterns = re.compile(r'sites|StanStatePublicDocs')
-relevant_links = soup.find_all(href=link_patterns)
+def store_page():
+    global soup
+    page = requests.get(department_address)
+    soup = BeautifulSoup(page.content, "html.parser")
 
-if relevant_links:
-    for relevant_link in relevant_links:
-        link_name.append(relevant_link.get_text())
-        link_address.append(relevant_link.get('href'))
+def compile_links():
+    global relevant_links
+    link_patterns = re.compile(r'sites|StanStatePublicDocs')
+    relevant_links = soup.find_all(href=link_patterns)
+
+def add_to_lists():
+    if relevant_links:
+        for relevant_link in relevant_links:
+            link_name.append(relevant_link.get_text())
+            link_address.append(relevant_link.get('href'))
+            migration_status.append(' ')
+            deletion_status.append(' ')
+    else:
+        link_name.append('No links present on this page.')
+        link_address.append(' ')
         migration_status.append(' ')
         deletion_status.append(' ')
-else:
-    link_name.append('No links present on this page.')
-    link_address.append(' ')
-    migration_status.append(' ')
-    deletion_status.append(' ')
+
+store_page()
+compile_links()
+add_to_lists()
 
 df = pd.DataFrame(link_name, columns = ['Link Name'])
 df['Link Address'] = link_address
@@ -40,3 +75,4 @@ file = os.path.join(file_path, excel_name)
 df = df.to_excel(file + ".xlsx")
 
 print("Downloaded: " + str(file))
+'''
