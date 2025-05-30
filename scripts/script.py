294d259b17705f5aaf11755be1e446bf608b4a7b
CacheOutDev <cache-out@outlook.com>
2025-05-30 13:46:10 -0700

    Sucess: Filtered nav links for department-specific nav menu link
    Collected all the links with the class: "nav-links" and filtered them to collect just the department-specific navigation menu links

--- scripts/script.py
+++ scripts/script.py
@@ -5,31 +5,37 @@ import numpy as np
 import re
 import openpyxl
 import os
+from collections.abc import Iterable
 
 department_address = input("Enter the department link address ")
 
 page = requests.get(department_address)
 soup = BeautifulSoup(page.content, "html.parser")
 
-nav_hrefs = []
-
 nav_links = soup.find_all(class_="nav-link")
+
+nav_hrefs = []
 
 for nav_link in nav_links:
     nav_hrefs.append(nav_link.get('href'))
 
-#print(nav_hrefs)
-
 parent_pages = []
 
+department_url = re.split(r'/', department_address)
+department_url = department_url.pop(3)
+department_url = "/" + department_url + "/"
+
 for nav_href in nav_hrefs:
-    if nav_href:
-       parent_pages.append(nav_href)
+    if type(nav_href) == str:
+        #print(nav_href)
+        #print(type(nav_href))
+        if department_url in nav_href:
+            parent_pages.append(nav_href)
     else:
         pass
-    print(nav_href)  
-
-print(parent_pages)
+    
+for parent_page in parent_pages:
+    print(parent_page)
 
 '''
 
@@ -45,7 +51,7 @@ def compile_links():
     link_patterns = re.compile(r'sites|StanStatePublicDocs')
     relevant_links = soup.find_all(href=link_patterns)
 
-def add_to_lists():
+def add():
     if relevant_links:
         for relevant_link in relevant_links:
             link_name.append(relevant_link.get_text())
@@ -60,7 +66,7 @@ def add_to_lists():
 
 store_page()
 compile_links()
-add_to_lists()
+add()
 
 df = pd.DataFrame(link_name, columns = ['Link Name'])
 df['Link Address'] = link_address
